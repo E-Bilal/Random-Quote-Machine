afebb4c8998d244b0363efe1bc6ad1cec20393b9
PC <atomium1994@gmail.com>
2021-07-31 17:34:58 +0200

    adds styling and fixes bugs

--- src/App.js
+++ src/App.js
@@ -8,40 +8,68 @@ class App extends React.Component {
       isLoaded: false,
     };
     this.componentDidMount = this.componentDidMount.bind(this);
+    this.getItemsFromApi = this.getItemsFromApi.bind(this);
   }
   componentDidMount() {
+    this.getItemsFromApi();
+  }
+
+  getItemsFromApi() {
     fetch("https://api.quotable.io/random")
       .then((response) => response.json())
       .then((json) => {
-        this.setState({
-          isLoaded: true,
-          items: json,
-        });
-      });
-    console.log(this.state);
+        if (json.length < 240) {
+          console.log(json.length);
+          this.setState({
+            isLoaded: true,
+            items: json,
+          });
+        } else {
+          this.getItemsFromApi();
+        }
+      })
+      .catch((err) => console.log(err));
   }
+
+  // hello = () => {
+  //   this. => APP class this
+  // }
+
+  // hello() {
+  //   this. => from caller this
+  // }
+
   render() {
-    let { isLoaded, items } = this.state;
+    // deconstruction ES6
+    // let { isLoaded, items } = this.state;
+
+    let isLoaded = this.state.isLoaded;
+    let items = this.state.items;
 
     if (!isLoaded) {
-      return <div></div>;
+      return <div>loading...</div>;
     } else {
       return (
         <div className="image">
           <div className="overlay">
             <div className="quotebox">
-              <p>{items.content}</p>
-              <p>{items.author}</p>
-              <button onClick={this.componentDidMount}>New Quote</button>
-              <a
-                className="twitter-share-button"
-                href={`https://twitter.com/intent/tweet?text=${items.content} -${items.author}`}
-                target="_blank"
-                rel=" noopener noreferrer"
-                data-size="large"
-              >
-                Tweet
-              </a>
+              <p className="quotetext">"{items.content}"</p>
+
+              <p className="author">- {items.author}</p>
+              <div className="container">
+                <a
+                  className="twitter"
+                  href={`https://twitter.com/intent/tweet?text="${items.content} -${items.author}`}
+                  target="_blank"
+                  rel=" noopener noreferrer"
+                  data-size="large"
+                >
+                  Tweet
+                </a>
+                <button className="button" onClick={this.getItemsFromApi}>
+                  New Quote
+                </button>
+              </div>
             </div>
           </div>
         </div>
